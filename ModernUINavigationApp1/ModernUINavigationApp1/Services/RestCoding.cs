4a1d79d8ef87231841ff5c2a2f6dd81f53ff1bf2
Maxime Mbabele <MMbabele@jhb.dvt.co.za>
2017-09-20 12:33:57 +0200

    simplify it to 1 project only

---
+++ ModernUINavigationApp1/ModernUINavigationApp1/Services/RestCoding.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+
+    public class CodingResult : BaseResult
+    {
+    }
+    public class RestCoding
+    {
+        public static CodingResult scoreCode(string lang, string source)
+        {
+            try
+            {   
+                Dictionary<string, string> data = new Dictionary<string, string>();
+                data["source"] = source;
+                data["language"] = lang;
+                string rs = RestClient.post("code", data);
+                Console.WriteLine(rs);
+                CodingResult r = JsonConvert.DeserializeObject<CodingResult>(rs);
+                Console.WriteLine(r);
+                return r;
+            }
+            catch (JsonException e)
+            {
+                Console.WriteLine(e);
+            }
+            return null;
+        }
+    }
+}
